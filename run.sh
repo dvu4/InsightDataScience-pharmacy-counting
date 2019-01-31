fcfc07a7f454142b06cde5579b96ce3002297440
Duc Vu <dvu4@uic.edu>
2019-01-31 01:48:31 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,5 +3,3 @@
 # Use this shell script to compile (if necessary) your code and then execute it. Below is an example of what might be found in this file if your program was written in Python
 #
 python ./src/pharmacy_counting.py ./input/itcont.txt ./output/top_cost_drug.txt
-#python ./src/pharmacy_counting.py ./insight_testsuite/tests/test_1/input/itcont.txt ./insight_testsuite/tests/test_1/output/top_cost_drug.txt
-#python ./src/pharmacy_counting.py ./insight_testsuite/tests/your-own-test_1/input/itcont.txt ./insight_testsuite/tests/your-own-test_1/output/top_cost_drug.txt
